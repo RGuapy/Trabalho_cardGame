1a3d3d8996d360acd9c8c301e0b7004008e20507
RGuapy <renanguapy@gmail.com>
2023-10-26 20:40:44 -0300

    veConstructorDepois

---
+++ front/src/model/CardModel.ts
@@ -0,0 +1,12 @@
+export interface CardModel{
+    
+    id: number,
+    dateCriacao: Date,
+    dateAlteracao: Date,
+    card_name: String,
+    card_type: String,
+    card_atk: number,
+    card_life: number,
+    card_desc: String
+    
+}
---
+++ front/src/service/CardService.ts
@@ -0,0 +1,9 @@
+import { CardModel } from "src/model/CardModel";
+import { BaseService } from "./BaseService";
+import { IResultHttp } from "src/interface/IResultHttp";
+import { HttpService } from "./HttpService";
+
+
+export class cardService extends BaseService<CardModel>{
+    //constructor('/cards',http:HttpService)
+}
